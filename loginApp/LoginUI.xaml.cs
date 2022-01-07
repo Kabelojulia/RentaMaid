c55db742edc61b727d8c80394eafd513e92347c1
kabelojulia <motloutsijk@gmail.com>
2022-01-07 15:46:19 +0200

    Add project files.

---
+++ loginApp/LoginUI.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace loginApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginUI : ContentPage
+    {
+        public LoginUI()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            //if(txtUsername.Text==admin && txtPassword.Text == "123")
+            // {
+
+            //  }
+            //else
+            //{
+            //DisplayAlert("Oops...""Username or password is incorrect!""OK);
+            //}
+            //Navigation.PushAsync(new Registration());
+            Navigation.PushAsync(new Menu());
+
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            
+          Navigation.PushAsync(new Registration());
+      
+
+        }
+    }
+    
+
+}
